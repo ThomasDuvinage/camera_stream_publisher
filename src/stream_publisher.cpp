6baf936cd2991902b4536e429c3cfedc837556d3
ThomasDuvinage <toto.duvinage@gmail.com>
2023-11-14 14:08:17 +0900

    Initial commit

---
+++ src/stream_publisher.cpp
@@ -0,0 +1,40 @@
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <sstream> // for converting the command line parameter to integer
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+
+  ros::init(argc, argv, "camera_stream_publihsher_node");
+  ros::NodeHandle nh("~");
+  image_transport::ImageTransport it(nh);
+  image_transport::Publisher pub = it.advertise("camera/image", 1);
+
+  int video_source;
+  nh.param<int>("video_source", video_source, 0);
+
+  cv::VideoCapture cap(video_source);
+  // Check if video device can be opened with the given index
+  if(!cap.isOpened()) return 1;
+  cv::Mat frame;
+  sensor_msgs::ImagePtr msg;
+
+  ros::Rate loop_rate(10);
+  while (nh.ok()) {
+    cap >> frame;
+    // Check if grabbed frame is actually full with some content
+    if(!frame.empty()) {
+      msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+      pub.publish(msg);
+      cv::waitKey(1);
+    }
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+}
