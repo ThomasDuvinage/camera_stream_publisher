3c17a29b79b69b73e489b30cbd63070b6b810b96
ThomasDuvinage <toto.duvinage@gmail.com>
2024-02-26 11:06:20 +0900

    [Streamer] add camera topic arg and update loop rate

--- src/stream_publisher.cpp
+++ src/stream_publisher.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <image_transport/image_transport.h>
@@ -13,25 +14,33 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "camera_stream_publisher_node");
   ros::NodeHandle nh("~");
   image_transport::ImageTransport it(nh);
-  image_transport::Publisher pub = it.advertise("image", 1);
 
   int video_source;
   nh.param<int>("video_source", video_source, 0);
 
+  std::string topic;
+  nh.param<std::string>("published_topic", topic, "image");
+
+  
+  image_transport::Publisher pub = it.advertise(topic, 100);
+
   cv::VideoCapture cap(video_source);
   // Check if video device can be opened with the given index
-  if(!cap.isOpened()) return 1;
+  if(!cap.isOpened()){
+    ROS_ERROR("Unable to open camera");
+    return EXIT_FAILURE;
+  }
+
   cv::Mat frame;
   sensor_msgs::ImagePtr msg;
 
-  ros::Rate loop_rate(10);
+  ros::Rate loop_rate(60);
   while (nh.ok()) {
     cap >> frame;
     // Check if grabbed frame is actually full with some content
     if(!frame.empty()) {
       msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
       pub.publish(msg);
-      cv::waitKey(1);
     }
 
     ros::spinOnce();
